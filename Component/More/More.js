07c10c378720354abb3873cfaee53bc134d74dec
hesf <1455941459@qq.com>
2016-11-28 18:24:09 +0800

    更多页面上边栏

--- Component/More/More.js
+++ Component/More/More.js
@@ -9,27 +9,63 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Platform,
+  Image,
+  TouchableOpacity
 } from 'react-native';
 
 var More = React.createClass({
   render() {
     return (
       <View style={styles.container}>
+        {/*导航条*/}
+        {this.renderNavBar()}
         <Text style={styles.welcome}>
          更多
         </Text>
  
       </View>
     );
+  },
+  renderNavBar()
+  {
+    return (
+        <View style={styles.navOutViewStyle}>
+            <Text style={{color:'white',fontSize:16,fontWeight:'bold'}}>更多</Text>
+             <TouchableOpacity onPress={()=>{alert("点击了");}} style={styles.rightViewStyle}>
+                <Image source={{uri:'icon_mine_setting'}} style={styles.navImageStyle}/>
+            </TouchableOpacity>
+        </View>
+      );
   }
+
 });
 
 const styles = StyleSheet.create({
+  navImageStyle:{
+    width:Platform.OS=='ios'?28:24,
+    height:Platform.OS=='ios'?28:24,
+   
+  },
+  rightViewStyle:{
+     // 绝对定位
+    position:'absolute',
+    right:10,
+    bottom:12
+  },
+  navOutViewStyle:{
+    height:Platform.OS=='ios'?64:44,
+    backgroundColor:'rgba(255,96,0,1.0)',
+    // 设置主轴的方向
+    flexDirection:'row',
+    //  垂直居中---->设置侧轴的对齐方向
+    alignItems:'center',
+    // 主轴方向居中
+    justifyContent:'center'
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
